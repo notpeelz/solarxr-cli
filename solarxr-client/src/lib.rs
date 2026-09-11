77e10e7f3ff658ab4e4292db81601ebff16bac36
peelz <peelz.dev@gmail.com>
2026-09-11 18:07:23 -0400

    fix: avoid duplicate fields in reset spans
    The reset_mounting/reset_with_parts wrappers recorded the same
    delay and wait_until_finished fields as the inner reset functions,
    causing tracing to print them twice. Only instrument the inner
    functions since they capture a superset of the wrapper arguments.

--- solarxr-client/src/lib.rs
+++ solarxr-client/src/lib.rs
@@ -449,7 +449,6 @@ impl<const BUF_SIZE: usize> SolarXRClient<BUF_SIZE> {
 
 macro_rules! impl_reset {
     ($name:ident; $reset_type:expr) => {
-        #[instrument(level = "trace", skip(self))]
         pub async fn $name(&self, delay: Duration, wait_until_finished: bool) -> Result<()> {
             self.reset($reset_type, delay, wait_until_finished).await
         }
@@ -459,7 +458,6 @@ macro_rules! impl_reset {
 macro_rules! impl_reset_with_parts {
     ($name:ident; $reset_type:expr) => {
         #[allow(dead_code)]
-        #[instrument(level = "trace", skip(self))]
         pub async fn $name(
             &self,
             body_parts: &[proto::datatypes::BodyPart],
